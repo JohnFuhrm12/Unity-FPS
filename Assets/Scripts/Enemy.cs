3bccc5f9f39ab59747d53d126c39a737b5e0b7c7
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-10-17 20:00:35 -0500

    Add files via upload

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,13 @@ public class Enemy : MonoBehaviour
     public float health = 3f;
     public float damageAmount = 1f;
 
+    public float movementSpeed = 1;
+
+    public bool seesPlayer = false;
+
+    //int MaxDist = 20;
+    int MinDist = 12;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +23,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject player = GameObject.Find("PlayerCapsule");
+
+        if (Vector3.Distance(transform.position, player.transform.position) >= MinDist) {
+            seesPlayer = true;
+        }
+
+        if (seesPlayer == true) {
+            transform.LookAt(player.transform.position);
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= MinDist) {
+            seesPlayer = false;
+        }
+
     }
 
     public void TakeDamage() {
